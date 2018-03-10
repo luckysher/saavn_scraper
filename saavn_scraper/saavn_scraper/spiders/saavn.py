9fa9d9887fbea008cf68e75c05ee12c36ccb95ff
luckysher <luckyshera95@gmail.com>
2018-03-10 13:05:56 +0530

    added empty method for fetching albums

--- saavn_scraper/saavn_scraper/spiders/saavn.py
+++ saavn_scraper/saavn_scraper/spiders/saavn.py
@@ -11,17 +11,20 @@ class SaavnSpider(scrapy.Spider):
     def __init__(self):
         self.loggerName = self.__class__.__name__
 
-    def fetchLatestMovieTitles(self, text):
+    def printAlbumDetails(self, album):
+        pass
+
+    def fetchLatestAlbums(self, text):
         num = 0
         title = ""
         artist = ""
         # fetch albums details
         albums_text = Selector(text=text).xpath('//div/album-grid').extract()
         albums_details = Selector(text=albums_text).xpath('//div/album-item').extract()
-        for album_detail in albums_details:
-
+        for num, album_detail in enumerate(albums_details):
+            num, title = num, Selector(text=albums_details).xpath('//h3/title/text()').extract_first()
 
 
     def parse(self, response):
         self.logger.debug("[%s] Fetching latest movie titles from 'www.saavn.com'" % self.loggerName)
-        self.fetchLatestMovieTitles(response.text)
+        self.fetchLatestAlbums(response.text)
