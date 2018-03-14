6826495e0d89c6ce994a5d1441cda1a5a67543d0
luckysher <luckyshera95@gmail.com>
2018-03-15 05:24:55 +0530

    added method for writing to json

--- saavn_scraper/saavn_scraper/pipelines.py
+++ saavn_scraper/saavn_scraper/pipelines.py
@@ -13,3 +13,9 @@ class AlbumPipeline(object):
     def open_spider(self, spider):
         print("opening spider...")
         self.file = open(output_json, 'wb')
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
+
